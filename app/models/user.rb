b25e6baf7c3548a88acac1bcdea3929fc076ba27
Michael Heu <mheu@clickbooq.com>
2012-09-05 12:43:43 -0400

    user.rb additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+
+  has_many :sites,   :dependent => :destroy
+  has_many :collections,  :dependent => :destroy
+
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false	
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
