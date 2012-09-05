4c704d0a076fe5627582f67b2918be26d3830e03
Michael Heu <mheu@clickbooq.com>
2012-09-05 16:16:53 -0400

    site scaffold

---
+++ app/models/site.rb
@@ -0,0 +1,11 @@
+class Site < ActiveRecord::Base
+  belongs_to :user
+  has_many :collections
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  attr_accessible :name#, :slug, :user_id
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_presence_of :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Imageshare::Application.routes.draw do
 
+  resources :sites
+
   devise_for :users
   resources :users, :only => [:index, :show]
   root :to => "home#index"
