e6da9cedfe02a1195ed962fbe7cd1f70e11a69c2
Michael Heu <mheu@clickbooq.com>
2012-09-07 07:22:18 -0400

    added new ENV keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET']
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_directory    = ENV['S3_BUCKET_NAME']
+    config.fog_public = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
